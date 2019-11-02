0ba9b2dc1be5bb7224454ecb167555a553625415
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2019-11-02 16:26:08 +0100

    refactor CustomerController

--- customer-service/src/main/kotlin/com/rewedigital/talks/customerservice/CustomerController.kt
+++ customer-service/src/main/kotlin/com/rewedigital/talks/customerservice/CustomerController.kt
@@ -1,5 +1,6 @@
 package com.rewedigital.talks.customerservice
 
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.net.URI
@@ -9,26 +10,42 @@ import java.net.URI
 class CustomerController(private val customerRepository: CustomerRepository) {
 
     @GetMapping("/customers/{id}")
-    fun getCustomer(@PathVariable id: String): ResponseEntity<CustomerDto> = customerRepository
-        .findById(id)
-        .map { ResponseEntity.ok(it.toDto()) }
-        .orElse(ResponseEntity.notFound().build())
+    fun getCustomer(@PathVariable id: String): ResponseEntity<CustomerGetResponse> =
+        customerRepository
+            .findById(id)
+            .map { ResponseEntity.ok(it.toGetResponse()) }
+            .orElse(ResponseEntity.notFound().build())
 
     @PostMapping("/customers")
-    fun createCustomer(@RequestBody customer: CustomerDto) = customerRepository
-        .save(customer.toEntity())
-        .createdResponse()
+    fun createCustomer(@RequestBody createRequest: CustomerCreateRequest): ResponseEntity<Unit> =
+        customerRepository
+            .save(createRequest.toEntity())
+            .toCreatedResponse()
 
-    private fun CustomerEntity.createdResponse() = ResponseEntity
-        .created(URI.create("/customers/${id!!}"))
-        .build<CustomerDto>()
+    private fun CustomerEntity.toCreatedResponse() =
+        ResponseEntity
+            .created(URI.create("/customers/${id!!}"))
+            .build<Unit>()
 
-    private fun CustomerEntity.toDto() = CustomerDto(id = id, name = name)
+    private fun CustomerEntity.toGetResponse() =
+        CustomerGetResponse(id = id!!, name = name)
+
+    private fun CustomerCreateRequest.toEntity() =
+        if (name == "Balrok")
+            throw UnprocessableEntityExcepction("You can not pass!")
+        else
+            CustomerEntity(name = name)
 
 }
 
-data class CustomerDto(val id: String?, val name: String) {
-    fun toEntity() = CustomerEntity(name = name)
-}
+data class CustomerCreateRequest(
+    val name: String
+)
 
+data class CustomerGetResponse(
+    val id: String,
+    val name: String
+)
 
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+class UnprocessableEntityExcepction(message: String) : Exception(message)
