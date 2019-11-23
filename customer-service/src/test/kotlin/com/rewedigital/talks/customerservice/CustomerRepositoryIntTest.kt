7c253340bd821a21368ad650c6ef2945e92eee51
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2019-11-23 17:48:42 +0100

    add test for duplicate key violation

--- customer-service/src/test/kotlin/com/rewedigital/talks/customerservice/CustomerRepositoryIntTest.kt
+++ customer-service/src/test/kotlin/com/rewedigital/talks/customerservice/CustomerRepositoryIntTest.kt
@@ -2,8 +2,10 @@ package com.rewedigital.talks.customerservice
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest
+import org.springframework.dao.DuplicateKeyException
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.jdbc.Sql
 import java.util.*
@@ -25,6 +27,17 @@ class CustomerRepositoryIntTest(
 
         assertThat(customerRepository.findById(customer.id)).isEqualTo(customer)
         assertThat(customerRepository.findById(someUUID())).isNull()
+    }
+
+    @Test
+    fun `save twice`() {
+        val sameId = someUUID()
+        customerRepository.save(Customer(id = sameId, name = "John Doe"))
+
+        assertThrows<DuplicateKeyException> {
+            customerRepository.save(Customer(id = sameId, name = "John Doe"))
+
+        }
     }
 
     private fun someUUID() = UUID.randomUUID()
